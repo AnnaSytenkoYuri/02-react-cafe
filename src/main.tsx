9edb56b3b4f018bf50eedfea9ea7912169253602
Anna Sytenko <anna_sytenko91@icloud.com>
2025-08-27 14:36:22 +0700

    practice

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./assets/components/App";
+// // Нормалізація стилів
+// import  "modern-normalize";
+// // Глобальні стилі (додатково)
+// import "./global.css";
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
+
+
+
+
